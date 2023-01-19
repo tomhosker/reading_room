519d5356438b24d76c05af2de8d741caa2bf95e5
tomhosker <tomdothosker@gmail.com>
2023-01-19 01:04:49 +0000

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Reading Room
+
+This code defines the server for the **Reading Room**, which is forms a kind of personal intranet.
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+/*
+Returns the home page.
+*/
+
+// Imports.
+const express = require("express");
+
+// Local imports.
+const Finaliser = require("../lib/finaliser.js");
+
+// Constants.
+const router = express.Router();
+const finaliser = new Finaliser();
+
+// GET home page.
+router.get("/", function (req, res, next) {
+    finaliser.protoRender(req, res, "index", { title: "Welcome" });
+});
+
+// Exports.
+module.exports = router;
