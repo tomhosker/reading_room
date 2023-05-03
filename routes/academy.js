b0f8a2b29ebbb05f9f5145f16b6a6060cad9439f
tomhosker <tomdothosker@gmail.com>
2023-05-04 00:48:53 +0100

    Continuing to add canon pages.

--- routes/academy.js
+++ routes/academy.js
@@ -10,10 +10,27 @@ const AcademyRetriever = require("../lib/retrievers/academy_retriever.js");
 const FacultyRetriever = require("../lib/retrievers/faculty_retriever.js");
 const DepartmentRetriever =
     require("../lib/retrievers/department_retriever.js");
+const {
+    HoskersAlmanackRetriever,
+    HoskersCatalogueRetriever,
+    HoskersAnthemsRetriever,
+    HoskersAlbumsRetriever,
+    HoskersCinemaRetriever,
+    HoskersTelevisionRetriever
+} = require("../lib/retrievers/canon_retriever.js");
 
-// Constants.
+// Local constant objects.
 const router = express.Router();
 
+// Local constants.
+const KEY_TO_RETRIEVER_CLASS = {
+    almanack: HoskersAlmanackRetriever,
+    catalogue: HoskersCatalogueRetriever,
+    anthems: HoskersAnthemsRetriever,
+    albums: HoskersAlbumsRetriever,
+    cinema: HoskersCinemaRetriever,
+    television: HoskersTelevisionRetriever
+};
 
 // Get the academy's page.
 router.get("/", function (req, res, next) {
@@ -34,6 +51,15 @@ router.get("/faculties/:id", function (req, res, next) {
 router.get("/departments/:id", function (req, res, next) {
     const key = req.params.id;
     const retriever = new DepartmentRetriever(req, res, key);
+
+    retriever.startHere();
+});
+
+// Get the page for a given canon.
+router.get("/canons/:id", function (req, res, next) {
+    const key = req.params.id;
+    const RetrieverClass = KEY_TO_RETRIEVER_CLASS[key];
+    const retriever = new RetrieverClass(req, res, key);
 
     retriever.startHere();
 });
