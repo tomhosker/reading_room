2ab0c98dd0c847b0c0c0f39d6e7cae49506b8ee5
tomhosker <tomdothosker@gmail.com>
2023-05-29 20:28:35 +0100

    Fixed Cyprian dates.

--- test/test_timings.js
+++ test/test_timings.js
@@ -6,17 +6,56 @@ Test the Timings class.
 const expect = require("expect");
 
 // Local imports.
-const Timings = require("../lib/timings.js");
+const { CyprianDate, Timings } = require("../lib/timings.js");
 
 /*************
  ** TESTING **
  ************/
 
+describe("Test CyprianDate", function () {
+    it("Test 01 Pri T1", function () {
+        const actual = new CyprianDate(new Date(2014, 3, 1)).toString();
+        const expected = '01 Pri <span class="frak">T</span><sub>1</sub>';
+
+        expect(actual).toEqual(expected);
+    });
+    it("Test 30 Pri T1", function () {
+        const actual = new CyprianDate(new Date(2014, 3, 30)).toString();
+        const expected = '30 Pri <span class="frak">T</span><sub>1</sub>';
+
+        expect(actual).toEqual(expected);
+    });
+    it("Test 01 Sec T1", function () {
+        const actual = new CyprianDate(new Date(2014, 4, 1)).toString();
+        const expected = '01 Sec <span class="frak">T</span><sub>1</sub>';
+
+        expect(actual).toEqual(expected);
+    });
+    it("Test 29 Duo T1", function () {
+        const actual = new CyprianDate(new Date(2015, 2, 20)).toString();
+        const expected = '29 Duo <span class="frak">T</span><sub>1</sub>';
+
+        expect(actual).toEqual(expected);
+    });
+    it("Test 01 Pri T2", function () {
+        const actual = new CyprianDate(new Date(2015, 2, 21)).toString();
+        const expected = '01 Pri <span class="frak">T</span><sub>2</sub>';
+
+        expect(actual).toEqual(expected);
+    });
+    it("Test 01 Int T2", function () {
+        const actual = new CyprianDate(new Date(2016, 2, 11)).toString();
+        const expected = '01 Int <span class="frak">T</span><sub>2</sub>';
+
+        expect(actual).toEqual(expected);
+    });
+});
+
 describe("Test Timings", function () {
     it("Test getNow", function () {
-        const expResult = (new Date()).toISOString();
+        const expected = (new Date()).toISOString();
         let timings = new Timings();
 
-        expect(timings.getNow()).toEqual(expResult);
+        expect(timings.getNow()).toEqual(expected);
     });
 });
