9d6875f59e6ccb06194e7c53760a17434e7470f7
tomhosker <tomdothosker@gmail.com>
2024-01-25 18:40:38 +0000

    Amended mega downloads local path.

--- python/source/paths.py
+++ python/source/paths.py
@@ -8,7 +8,7 @@ from pathlib import Path
 # Path objects.
 PATH_OBJ_TO_READING_ROOM = Path.home()/"reading_room"
 PATH_OBJ_TO_DB_DIR = PATH_OBJ_TO_READING_ROOM/"databases"
-PATH_OBJ_TO_MEGA_DOWNLOADS = Path.home()/"mega_downloads"
+PATH_OBJ_TO_MEGA_DOWNLOADS = Path.home()/"Downloads"/"mega_downloads"
 
 # Paths.
 PATH_TO_READING_ROOM = str(PATH_OBJ_TO_READING_ROOM)
